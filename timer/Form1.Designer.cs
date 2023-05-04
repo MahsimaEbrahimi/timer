debbcd1537efe999458ffc175ccb2ceaa0219c0d
unknown <mahsimaeb83@gmail.com>
2023-05-04 15:09:23 +0430

    this is a timer

---
+++ timer/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.show_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // show_btn
+            // 
+            this.show_btn.Location = new System.Drawing.Point(493, 38);
+            this.show_btn.Name = "show_btn";
+            this.show_btn.Size = new System.Drawing.Size(76, 56);
+            this.show_btn.TabIndex = 0;
+            this.show_btn.Text = "show";
+            this.show_btn.UseVisualStyleBackColor = true;
+            this.show_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 411);
+            this.Controls.Add(this.show_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button show_btn;
+    }
+}
+
---
+++ timer/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            make_lable();
+
+        }
+        public void make_lable()
+        {
+            int top, left, height, width;
+            top = this.Height / 10;
+            left = this.Width / 10;
+            height = this.Height / 10;
+            width = this.Width / 10;
+            make_buttom(top,left,height,width);
+            Label t = new Label();
+            t.Top = top;
+            t.Left = 4 * left;
+            t.Name = "txtdisplay";
+            t.Text = "its a timer";
+            this.Controls.Add(t);
+        }
+        public void make_buttom(int top, int left, int height, int width)
+        {
+            for(int i =0;i<=2;i++)
+            {
+                Button b = new Button();
+                b.Top = 4*top;
+                b.Left = (3+i)*left;
+                b.Height = height;
+                b.Width = width;
+                this.Controls.Add(b);
+            }
+        }
+
+    }
+}
