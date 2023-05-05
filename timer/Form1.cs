a3d70182057a1e75fb248e867c6ea698031e9621
unknown <mahsimaeb83@gmail.com>
2023-05-05 20:18:13 +0430

    file

--- timer/Form1.cs
+++ timer/Form1.cs
@@ -20,6 +20,7 @@ namespace timer
         private void button1_Click(object sender, EventArgs e)
         {
             make_lable();
+            make_timer();
 
         }
         public void make_lable()
@@ -29,9 +30,11 @@ namespace timer
             left = this.Width / 10;
             height = this.Height / 10;
             width = this.Width / 10;
+
             make_buttom(top,left,height,width);
             Label t = new Label();
-            t.Top = top;
+
+            t.Top = 2*top;
             t.Left = 4 * left;
             t.Name = "txtdisplay";
             t.Text = "its a timer";
@@ -39,15 +42,21 @@ namespace timer
         }
         public void make_buttom(int top, int left, int height, int width)
         {
-            for(int i =0;i<=2;i++)
+            string[] names_button = {"reset","continue","pause"};
+            for(int i =1;i<=3;i++)
             {
                 Button b = new Button();
+                b.Text = names_button[i-1];
                 b.Top = 4*top;
-                b.Left = (3+i)*left;
                 b.Height = height;
-                b.Width = width;
+                b.Width = 2*width;
+                b.Left = ((2*i)*left);
                 this.Controls.Add(b);
             }
+        }
+        public void make_timer()
+        {
+
         }
 
     }
